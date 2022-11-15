fcb90340187aabb50a13431c878df3539cb95b8e
준석 <112670899+artofministori@users.noreply.github.com>
2022-11-15 06:51:24 +0000

    add

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 #include "05_loop.h"
 #include "06_selection.h"
 
+void my(){
+  
+}
+
+int a;
+
 int main() {
   cout << "Hello World!\n";
 
+  a = 10;
   // 04 Complex Data
   // complexDataStructure();
   // complexDataEnum();
@@ -15,7 +22,7 @@ int main() {
   // 05 loop
   // loopIncDec();
   // compareString();
-
+  my();
   // 06 selection
   // enumSwitch();
   // outfile();
